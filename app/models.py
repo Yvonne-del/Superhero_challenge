cd5427233b4597643cb5d88830f45d3693367dc3
Yvonne <ys359375@gmail.com>
2025-06-20 01:32:30 +0300

    update models and migrate

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Enum
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__= 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -15,21 +17,22 @@ class Hero(db.Model):
 
     heropowers = db.relationship('HerePower', back_populates='hero')
 
-class Power(db.Model):
+class Power(db.Model,SerializerMixin):
     __tablename__ = 'powers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    description= db.Column(db.String)
+    description= db.Column(db.String(20), nullable=False)
 
     heropowers = db.relationship('HerePower', back_populates='power')
 
 
-class HeroPower(db.Model):
+class HeroPower(db.Model, SerializerMixin):
     __tablename__='heropowers'
 
     id= db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
+    strength = db.Column(Enum('Strong', 'Weak', 'Average', name='strength_enum'), nullable=False)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
 
